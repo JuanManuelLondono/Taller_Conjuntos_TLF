cb47a4e45858ea8cb534d0aa14ee85340e59ae72
JuanSamuelArbelaez <109239223+JuanSamuelArbelaez@users.noreply.github.com>
2025-08-28 13:49:36 -0500

    Innitial Commit

---
+++ main.py
@@ -0,0 +1,69 @@
+from collections.abc import Iterable
+
+
+def practica_punto_1():
+    #Cree un conjunto frutas con al menos 5 frutas.
+    frutas = set({"Banana", "Kiwi", "Manzana", "Mora", "Maracuya"})
+
+    #Agregue una fruta nueva.
+    frutas.add("Sandia")
+
+    #Elimine una fruta existente.
+    frutas.discard("Kiwi")
+
+    #Intente agregar un elemento repetido y muestre el conjunto final.
+    frutas.add("Banana")
+    print(frutas)
+
+
+def practica_punto_2():
+    #Cree 2 conjuntos
+    estudiantes_matematicas = {"Ana", "Luis", "Carlos", "María"}
+    estudiantes_fisica = {"Carlos", "María", "Sofía", "Pedro"}
+    
+    #Muestra los estudiantes que están en ambas clases.
+    print(estudiantes_fisica.intersection(estudiantes_matematicas))
+    
+    #Muestra los estudiantes que están en matemáticas pero no en física.
+    print(estudiantes_matematicas.difference(estudiantes_fisica))
+
+    #Muestra la unión de estudiantes.
+    print(estudiantes_matematicas.union(estudiantes_fisica))
+
+    #Muestra los estudiantes que están solo en una de las dos clases.
+    print(estudiantes_fisica.symmetric_difference(estudiantes_matematicas))
+
+
+def practica_punto_3():
+    lista = [1, 2, 2, 3, 4, 4, 5]
+    print(eliminar_duplicados(lista))
+
+
+def eliminar_duplicados(*s: Iterable[any]) -> list[any]:
+    #Recibe una lista con elementos repetidos y devuelve una lista sin duplicados.
+    return list(set().union(*s))
+
+
+def practica_punto_4():
+    #Pide al usuario que ingrese un párrafo de texto. Convierte el texto a minúsculas.
+    parrafo = input("Ingrese su parrafo de texto: ").lower()
+    
+    #Elimina los signos de puntuación.
+    puntuaciones = ".,();"
+    parrafo = parrafo.translate(str.maketrans("", "", puntuaciones))
+
+    #Guarda en un conjunto todas las palabras únicas
+    words = set(parrafo.split())
+
+    #Muestra el número de palabras únicas
+    print("Cantidad de palabras únicas:", len(words))
+
+    #Muestra la lista ordenada de palabras únicas
+    print("Palabras ordenadas:", sorted(words))
+
+
+if __name__ == "__main__":
+    practica_punto_1()
+    practica_punto_2()
+    practica_punto_3()
+    practica_punto_4()
